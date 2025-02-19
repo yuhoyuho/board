a0ef61180f89bba31e6aeac32fb5bc0039949bd6
yuhoyuho <leeyuho123@naver.com>
2025-02-19 21:22:49 +0900

    상세 조회 기능 추가, DB에 update 되도록 변경

--- src/main/java/com/example/board/Service/BoardService.java
+++ src/main/java/com/example/board/Service/BoardService.java
@@ -5,9 +5,11 @@ import com.example.board.Entity.BoardEntity;
 import com.example.board.Repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +17,7 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
 
+    @Transactional
     public void save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
@@ -29,5 +32,23 @@ public class BoardService {
         }
 
         return boardDTOList;
+    }
+
+    @Transactional // 메소드를 만들어서 사용하는 경우 이 애너테이션을 붙이지 않으면 에러 발생
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+
+        if(optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+
+            return BoardDTO.toBoardDTO(boardEntity);
+        }
+        else {
+            return null;
+        }
     }
 }
