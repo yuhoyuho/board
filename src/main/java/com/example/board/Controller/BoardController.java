0b8cf1b9063ea2866f237c9b472e2b93658c6f07
yuhoyuho <leeyuho123@naver.com>
2025-02-19 17:21:05 +0900

    DTO 생성

--- src/main/java/com/example/board/Controller/BoardController.java
+++ src/main/java/com/example/board/Controller/BoardController.java
@@ -1,8 +1,8 @@
 package com.example.board.Controller;
 
+import com.example.board.DTO.BoardDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/board")
@@ -11,5 +11,10 @@ public class BoardController {
     @GetMapping("/save")
     public String saveForm() {
         return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        return null;
     }
 }
---
+++ src/main/java/com/example/board/DTO/BoardDTO.java
@@ -0,0 +1,26 @@
+package com.example.board.DTO;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+// DTO (Data Transfer Object : 데이터를 전송할 때 사용하는 객체)
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자
+public class BoardDTO {
+
+    private Long id;
+    private String boardWriter;
+    private String boardPass;
+    private String boardTitle;
+    private String boardContent;
+    private int boardHits; // 조회 수
+    private LocalDateTime boardCreatedTime;
+    private LocalDateTime boardUpdatedTime;
+
+
+}
