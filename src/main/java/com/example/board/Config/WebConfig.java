271c4d63f0bbefaf08fb6e32adb767a44affd66c
yuhoyuho <leeyuho123@naver.com>
2025-02-20 18:11:42 +0900

    savePath 경로 수정

--- src/main/java/com/example/board/Config/WebConfig.java
+++ src/main/java/com/example/board/Config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private String resourcePath = "/upload/**"; // view에서 접근하는 경로
-    private String savePath = "file:///C:/spring_boot_img/"; // 실제로 저장된 파일의 경로
+    private String savePath = "file:///C:/springboot_img/"; // 실제로 저장된 파일의 경로
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
